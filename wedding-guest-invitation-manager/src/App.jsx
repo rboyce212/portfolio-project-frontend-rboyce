381de3bc3ad9b45dcd4cff03c1e97d7cd353d6fa
Rich Boyce <122512687+rboyce212@users.noreply.github.com>
2023-11-19 23:50:06 -0500

    set up app.js page

--- wedding-guest-invitation-manager/src/App.jsx
+++ wedding-guest-invitation-manager/src/App.jsx
@@ -1,17 +1,38 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Edit from "./Pages/Edit";
+import Error from "./Pages/Error";
+import Home from "./Pages/Home";
+import Index from "./Pages/Index";
+import Add from "./Pages/Add";
+import Details from "./Pages/Details";
+import NavBar from "./Components/NavBar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="App">
+      <Router>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/guests" element={<Index />} />
+            <Route path="/guests/add" element={<Add />} />
+            <Route path="/guests/:index" element={<Details />} />
+            <Route path="/guests/:index/edit" element={<Edit />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </main>
+      </Router>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           Visitors: {count}
         </button>
       </div>
-    </>
+    </div>
   );
 }
 
